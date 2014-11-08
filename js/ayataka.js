5299531593ba56830ad4beec6fe05ffa3e29ecf9
niba1122 <niba1122@gmail.com>
2014-11-09 08:29:59 +0900

    first upload

---
+++ js/ayataka.js
@@ -0,0 +1,12 @@
+var Ayataka = {
+	getJSONP: function (jsonp_callback, url, callback) {
+		$.ajax({
+			type: 'GET',
+			url: url,
+			dataType: 'jsonp',
+			jsonpCallback: jsonp_callback,
+			success: callback,
+			error: function (XMLHttpRequest, textStatus, errorThrown){ console.log(textStatus); }
+		});
+	}
+}

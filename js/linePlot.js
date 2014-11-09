8baec1ef9e364c5ff95dbc0a86baffe2d9fb1103
niba1122 <niba1122@gmail.com>
2014-11-09 11:50:34 +0900

    moduled and merged

--- js/linePlot.js
+++ js/linePlot.js
@@ -1,61 +1,64 @@
-var width = 600;
-var height = 400;
-var xPadding = 40;
-var yPadding = 40;
+var LinePlot = {
+	width: null,
+	height: null,
+	xPadding: null,
+	yPadding: null,
+	svg: null,
+	xAxisScale: null,
+	yAxisScale: null,
+	xAxis: null,
+	yAxis: null,
+	xAxisGroup: null,
+	yAxisGroup: null,
+	points: [[1,4],[2,2],[3,5],[4,9],[5,3],[6,1],[7,5],[8,4],[9,6],[10,7],[11,23],[12,11]],
+	line: null,
+	path: null,
+	markers: null,
+	labels: null,
+	show: function () {
+		this.width = 600;
+		this.height = 400;
+		this.xPadding = 40;
+		this.yPadding = 40;
 
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height).attr("id", "svgTag").style("border","1px solid #eee");
+		this.svg = d3.select("body").append("svg").attr("width", this.width).attr("height", this.height).attr("id", "svgTag").style("border","1px solid #eee");
 
-var xAxisScale = d3.scale.linear().domain([1,12]).range([0+xPadding,width-xPadding]);
-var yAxisScale = d3.scale.linear().domain([0,20]).range([height-yPadding,0+yPadding]);
+		this.xAxisScale = d3.scale.linear().domain([1,12]).range([0+this.xPadding,this.width-this.xPadding]);
+		this.yAxisScale = d3.scale.linear().domain([0,20]).range([this.height-this.yPadding,0+this.yPadding]);
 
-var xAxis = d3.svg.axis().scale(xAxisScale);
-var yAxis = d3.svg.axis().scale(yAxisScale).orient("left");
+		this.xAxis = d3.svg.axis().scale(this.xAxisScale);
+		this.yAxis = d3.svg.axis().scale(this.yAxisScale).orient("left");
 
-var xAxisGroup = svg.append("g").attr("class","x axis").attr("transform", "translate(0," + (height-yPadding) + ")").call(xAxis);
-var yAxisGroup = svg.append("g").attr("class","y axis").attr("transform", "translate(" + xPadding + ",0)").call(yAxis);
+		this.xAxisGroup = this.svg.append("g").attr("class","x axis").attr("transform", "translate(0," + (this.height-this.yPadding) + ")").call(this.xAxis);
+		this.yAxisGroup = this.svg.append("g").attr("class","y axis").attr("transform", "translate(" + this.xPadding + ",0)").call(this.yAxis);
 
+		this.line=d3.svg.line().x(function(d){ return LinePlot.xAxisScale(d[0])}).y(function(d){return LinePlot.yAxisScale(d[1])});
+		this.path=this.svg.append("g").append("path").attr("id","pathEntry").datum(this.points).attr("class","line").attr(
+		    {
+ 		       d: this.line,
+		        fill: "none"
+		    });
 
+		this.markers=this.svg.append("g").attr("id","markerGroup").selectAll("circle").data(this.points).enter().append("circle").attr(
+		    {
+		        cx: function(d){return LinePlot.xAxisScale(d[0])},
+		        cy: function(d){return LinePlot.yAxisScale(d[1])},
+		        r: 5,
+		        fill: "magenta"
+		    }).on("click",
+		        function(d,i){
+            		d3.select(this).attr("fill", "black");
+		        }
+		    );
 
-var points=[[1,4],[2,2],[3,5],[4,9],[5,3],[6,1],[7,5],[8,4],[9,6],[10,7],[11,23],[12,11]];
-
-
-
-
-
-
-var line=d3.svg.line().x(function(d){ return xAxisScale(d[0])}).y(function(d){return yAxisScale(d[1])});
-var path=svg.append("g").append("path").attr("id","pathEntry").datum(points).attr("class","line").attr(
-    {
-        d: line,
-        fill: "none"
-    });
-
-var markers=svg.append("g").attr("id","markerGroup").selectAll("circle").data(points).enter().append("circle").attr(
-    {
-        cx: function(d){return xAxisScale(d[0])},
-        cy: function(d){return yAxisScale(d[1])},
-        r: 5,
-        fill: "magenta"
-    }).on("click",
-        function(d,i){
-            d3.select(this).attr("fill", "black");
-        }
-    );
-
-var labels=svg.append("g").selectAll("text").data(points).enter().append("text").attr({
-        class: "dataPointLabels",
-        x: function(d){return xAxisScale(d[0])},
-        y: function(d){return yAxisScale(d[1])}
-    }).text(
-        function(d){return "　"+d[1]+" "}
-    );
-
-buttonClicked=function (){
-    var svg = document.getElementById("svgTag");
-    var svg_xml = (new XMLSerializer).serializeToString(svg);
-
-    d3.select("textarea").text(
-        svg_xml
-    );
-
+		this.labels=this.svg.append("g").selectAll("text").data(this.points).enter().append("text").attr({
+		        class: "dataPointLabels",
+		        x: function(d){return LinePlot.xAxisScale(d[0])},
+		        y: function(d){return LinePlot.yAxisScale(d[1])}
+		    }).text(
+		        function(d){return "　"+d[1]+" "}
+		    );
+		   
+	}
+	
 };
